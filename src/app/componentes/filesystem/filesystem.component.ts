c1f95883c9c77ec8b34a33be44187c9eebe2bf31
edwinparrales <edwinparrales82@gmail.com>
2024-03-23 00:54:13 -0500

    adicionando file systems

--- src/app/componentes/filesystem/filesystem.component.ts
+++ src/app/componentes/filesystem/filesystem.component.ts
@@ -1,16 +1,20 @@
 import {Component, inject} from '@angular/core';
 import {MediaService} from "../../servicios/media.service";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-filesystem',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './filesystem.component.html',
   styleUrl: './filesystem.component.css'
 })
 export class FilesystemComponent {
 
   //mediaservice:MediaService= inject(MediaService);
+  urlImagen:string='';
   constructor(private  mediaService:MediaService) {
   }
 
@@ -19,7 +23,12 @@ export class FilesystemComponent {
     if (file){
       const formData:FormData = new FormData();
       formData.append('file',file);
-      this.mediaService.uploadFile(formData).subscribe(res=> console.log('respuesta',res));
+      this.mediaService.uploadFile(formData).subscribe(res => {
+          console.log('respuesta', res.url)
+        this.urlImagen=res.url;
+
+        }
+      );
 
 
 
